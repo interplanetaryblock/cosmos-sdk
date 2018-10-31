b22b25682227947c41eb7f3dd97a4a13fb955735
zhangzirong <zhangzirong@didichuxing.com>
2018-10-31 17:09:43 +0800

    add Genesis Tx and State in AppInit phase

--- examples/multicoin/types/account.go
+++ examples/multicoin/types/account.go
@@ -1,9 +1,31 @@
 package types
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/spf13/pflag"
+	"github.com/tendermint/tendermint/crypto"
+	tmtypes "github.com/tendermint/tendermint/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/server/config"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/stake"
+)
+
+// DefaultKeyPass contains the default key password for genesis transactions
+const DefaultKeyPass = "multicoin"
+
+var (
+	// bonded tokens given to genesis validators/accounts
+	freeFermionVal  = int64(100)
+	freeFermionsAcc = int64(50)
 )
 
 var _ auth.Account = (*AppAccount)(nil)
@@ -48,7 +70,8 @@ func GetAccountDecoder(cdc *wire.Codec) auth.AccountDecoder {
 
 // GenesisState reflects the genesis state of the application.
 type GenesisState struct {
-	Accounts []*GenesisAccount `json:"accounts"`
+	Accounts  []GenesisAccount   `json:"accounts"`
+	StakeData stake.GenesisState `json:"stake"`
 }
 
 // GenesisAccount reflects a genesis account the application expects in it's
@@ -61,11 +84,11 @@ type GenesisAccount struct {
 
 // NewGenesisAccount returns a reference to a new GenesisAccount given an
 // AppAccount.
-func NewGenesisAccount(aa *AppAccount) *GenesisAccount {
-	return &GenesisAccount{
-		Name:    aa.Name,
-		Address: aa.Address,
-		Coins:   aa.Coins.Sort(),
+func NewGenesisAccount(acc *auth.BaseAccount) GenesisAccount {
+	return GenesisAccount{
+		//Name:    acc.Name,
+		Address: acc.Address,
+		Coins:   acc.Coins.Sort(),
 	}
 }
 
@@ -78,4 +101,174 @@ func (ga *GenesisAccount) ToAppAccount() (acc *AppAccount, err error) {
 			Coins:   ga.Coins.Sort(),
 		},
 	}, nil
+}
+
+// MultiAppInit get app init parameters for server init command
+func MultiAppInit() server.AppInit {
+	fsAppGenState := pflag.NewFlagSet("", pflag.ContinueOnError)
+
+	fsAppGenTx := pflag.NewFlagSet("", pflag.ContinueOnError)
+	fsAppGenTx.String(server.FlagName, "", "validator moniker, required")
+	fsAppGenTx.String(server.FlagClientHome, os.ExpandEnv("$HOME/.multicoind"),
+		"home directory for the client, used for key generation")
+	fsAppGenTx.Bool(server.FlagOWK, false, "overwrite the accounts created")
+
+	return server.AppInit{
+		FlagsAppGenState: fsAppGenState,
+		FlagsAppGenTx:    fsAppGenTx,
+		AppGenTx:         MultiAppGenTx,
+		AppGenState:      MultiAppGenStateJSON,
+	}
+}
+
+// MultiGenTx define simple genesis tx
+type MultiGenTx struct {
+	Name    string         `json:"name"`
+	Address sdk.AccAddress `json:"address"`
+	PubKey  string         `json:"pub_key"`
+}
+
+// MultiAppGenTx generates a Multicoin genesis transaction.
+func MultiAppGenTx(
+	cdc *wire.Codec, pk crypto.PubKey, genTxConfig config.GenTx,
+) (appGenTx, cliPrint json.RawMessage, validator tmtypes.GenesisValidator, err error) {
+	if genTxConfig.Name == "" {
+		return nil, nil, tmtypes.GenesisValidator{}, errors.New("Must specify --name (validator moniker)")
+	}
+
+	buf := client.BufferStdin()
+	prompt := fmt.Sprintf("Password for account '%s' (default %s):", genTxConfig.Name, DefaultKeyPass)
+
+	keyPass, err := client.GetPassword(prompt, buf)
+	if err != nil && keyPass != "" {
+		// An error was returned that either failed to read the password from
+		// STDIN or the given password is not empty but failed to meet minimum
+		// length requirements.
+		return appGenTx, cliPrint, validator, err
+	}
+
+	if keyPass == "" {
+		keyPass = DefaultKeyPass
+	}
+
+	addr, secret, err := server.GenerateSaveCoinKey(
+		genTxConfig.CliRoot,
+		genTxConfig.Name,
+		keyPass,
+		genTxConfig.Overwrite,
+	)
+	if err != nil {
+		return appGenTx, cliPrint, validator, err
+	}
+
+	mm := map[string]string{"secret": secret}
+	bz, err := cdc.MarshalJSON(mm)
+	if err != nil {
+		return appGenTx, cliPrint, validator, err
+	}
+
+	cliPrint = json.RawMessage(bz)
+	appGenTx, _, validator, err = MultiAppGenTxNF(cdc, pk, addr, genTxConfig.Name)
+
+	return appGenTx, cliPrint, validator, err
+}
+
+// MultiAppGenTxNF generate a Multicoin genesis transaction without flags
+func MultiAppGenTxNF(cdc *wire.Codec, pk crypto.PubKey, addr sdk.AccAddress, name string) (
+	appGenTx, cliPrint json.RawMessage, validator tmtypes.GenesisValidator, err error) {
+
+	var bz []byte
+	multiGenTx := MultiGenTx{
+		Name:    name,
+		Address: addr,
+		PubKey:  sdk.MustBech32ifyAccPub(pk),
+	}
+	bz, err = wire.MarshalJSONIndent(cdc, multiGenTx)
+	if err != nil {
+		return
+	}
+	appGenTx = json.RawMessage(bz)
+
+	validator = tmtypes.GenesisValidator{
+		PubKey: pk,
+		Power:  freeFermionVal,
+	}
+	return
+}
+
+// MultiAppGenState Create the core parameters for genesis initialization for multicoin
+// note that the pubkey input is this machines pubkey
+func MultiAppGenState(cdc *wire.Codec, appGenTxs []json.RawMessage) (genesisState GenesisState, err error) {
+
+	if len(appGenTxs) == 0 {
+		err = errors.New("must provide at least genesis transaction")
+		return
+	}
+
+	// start with the default staking genesis state
+	stakeData := stake.DefaultGenesisState()
+
+	// get genesis flag account information
+	genaccs := make([]GenesisAccount, len(appGenTxs))
+	for i, appGenTx := range appGenTxs {
+
+		var genTx MultiGenTx
+		err = cdc.UnmarshalJSON(appGenTx, &genTx)
+		if err != nil {
+			return
+		}
+
+		// create the genesis account, give'm few steaks and a buncha token with there name
+		accAuth := auth.NewBaseAccountWithAddress(genTx.Address)
+		accAuth.Coins = sdk.Coins{
+			{genTx.Name + "Token", sdk.NewInt(1000)},
+			{"steak", sdk.NewInt(freeFermionsAcc)},
+		}
+		acc := NewGenesisAccount(&accAuth)
+		genaccs[i] = acc
+		stakeData.Pool.LooseTokens = stakeData.Pool.LooseTokens.Add(sdk.NewRat(freeFermionsAcc)) // increase the supply
+
+		// add the validator
+		if len(genTx.Name) > 0 {
+			desc := stake.NewDescription(genTx.Name, "", "", "")
+			validator := stake.NewValidator(genTx.Address,
+				sdk.MustGetAccPubKeyBech32(genTx.PubKey), desc)
+
+			stakeData.Pool.LooseTokens = stakeData.Pool.LooseTokens.Add(sdk.NewRat(freeFermionVal)) // increase the supply
+
+			// add some new shares to the validator
+			var issuedDelShares sdk.Rat
+			validator, stakeData.Pool, issuedDelShares = validator.AddTokensFromDel(stakeData.Pool, freeFermionVal)
+			stakeData.Validators = append(stakeData.Validators, validator)
+
+			// create the self-delegation from the issuedDelShares
+			delegation := stake.Delegation{
+				DelegatorAddr: validator.Owner,
+				ValidatorAddr: validator.Owner,
+				Shares:        issuedDelShares,
+				Height:        0,
+			}
+
+			stakeData.Bonds = append(stakeData.Bonds, delegation)
+		}
+	}
+
+	// create the final app state
+	genesisState = GenesisState{
+		Accounts:  genaccs,
+		StakeData: stakeData,
+	}
+	return
+}
+
+// MultiAppGenStateJSON but with JSON
+func MultiAppGenStateJSON(cdc *wire.Codec, appGenTxs []json.RawMessage) (appState json.RawMessage, err error) {
+
+	// create the final app state
+	genesisState, err := MultiAppGenState(cdc, appGenTxs)
+	if err != nil {
+		return nil, err
+	}
+	appState, err = wire.MarshalJSONIndent(cdc, genesisState)
+	return
 }
