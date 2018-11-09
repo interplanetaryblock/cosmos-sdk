4739f4e676b268f4cb38b8ffaedef11d37ab17d0
zhangzirong <zhangzirong@didichuxing.com>
2018-11-09 16:56:27 +0800

    combine the storeKey from three to one

--- x/naming/keeper.go
+++ x/naming/keeper.go
@@ -1,6 +1,8 @@
 package naming
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/wire"
 	"github.com/cosmos/cosmos-sdk/x/bank"
@@ -10,55 +12,62 @@ import (
 type Keeper struct {
 	coinKeeper bank.Keeper
 
-	namesStoreKey  sdk.StoreKey // The (unexposed) key used to access the store from the Context.
-	ownersStoreKey sdk.StoreKey // The (unexposed) key used to access the store from the Context.
-	pricesStoreKey sdk.StoreKey // The (unexposed) key used to access the store from the Context.
+	storeKey sdk.StoreKey // The (unexposed) key used to access the store from the Context.
 
 	cdc *wire.Codec // The wire codec for binary encoding/decoding.
 }
 
+var (
+	resolvePrefix = "resolve_"
+	ownerPrefix   = "owner_"
+	pricePrefix   = "price_"
+)
+
 // NewKeeper creates new instances of the naming Keeper
-func NewKeeper(coinKeeper bank.Keeper, namesStoreKey sdk.StoreKey, ownersStoreKey sdk.StoreKey, priceStoreKey sdk.StoreKey, cdc *wire.Codec) Keeper {
+func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *wire.Codec) Keeper {
 	return Keeper{
-		coinKeeper:     coinKeeper,
-		namesStoreKey:  namesStoreKey,
-		ownersStoreKey: ownersStoreKey,
-		pricesStoreKey: priceStoreKey,
-		cdc:            cdc,
+		coinKeeper: coinKeeper,
+		storeKey:   storeKey,
+		cdc:        cdc,
 	}
 }
 
 // ResolveName - returns the string that the name resolves to
 func (k Keeper) ResolveName(ctx sdk.Context, name string) string {
-	store := ctx.KVStore(k.namesStoreKey)
-	bz := store.Get([]byte(name))
+	store := ctx.KVStore(k.storeKey)
+	key := fmt.Sprintf("%s%s", resolvePrefix, name)
+	bz := store.Get([]byte(key))
 	return string(bz)
 }
 
 // SetName - sets the value string that a name resolves to
 func (k Keeper) SetName(ctx sdk.Context, name string, value string) {
-	store := ctx.KVStore(k.namesStoreKey)
-	store.Set([]byte(name), []byte(value))
+	store := ctx.KVStore(k.storeKey)
+	key := fmt.Sprintf("%s%s", resolvePrefix, name)
+	store.Set([]byte(key), []byte(value))
 }
 
 // HasOwner - returns whether or not the name already has an owner
 func (k Keeper) HasOwner(ctx sdk.Context, name string) bool {
-	store := ctx.KVStore(k.ownersStoreKey)
-	bz := store.Get([]byte(name))
+	store := ctx.KVStore(k.storeKey)
+	key := fmt.Sprintf("%s%s", ownerPrefix, name)
+	bz := store.Get([]byte(key))
 	return bz != nil
 }
 
 // GetOwner - get the current owner of a name
 func (k Keeper) GetOwner(ctx sdk.Context, name string) sdk.AccAddress {
-	store := ctx.KVStore(k.ownersStoreKey)
-	bz := store.Get([]byte(name))
+	store := ctx.KVStore(k.storeKey)
+	key := fmt.Sprintf("%s%s", ownerPrefix, name)
+	bz := store.Get([]byte(key))
 	return bz
 }
 
 // SetOwner - sets the current owner of a name
 func (k Keeper) SetOwner(ctx sdk.Context, name string, owner sdk.AccAddress) {
-	store := ctx.KVStore(k.ownersStoreKey)
-	store.Set([]byte(name), owner)
+	store := ctx.KVStore(k.storeKey)
+	key := fmt.Sprintf("%s%s", ownerPrefix, name)
+	store.Set([]byte(key), owner)
 }
 
 // GetPrice - gets the current price of a name.  If price doesn't exist yet, set to 1steak.
@@ -66,8 +75,9 @@ func (k Keeper) GetPrice(ctx sdk.Context, name string) sdk.Coins {
 	if !k.HasOwner(ctx, name) {
 		return sdk.Coins{sdk.NewInt64Coin("mycoin", 1)}
 	}
-	store := ctx.KVStore(k.pricesStoreKey)
-	bz := store.Get([]byte(name))
+	store := ctx.KVStore(k.storeKey)
+	key := fmt.Sprintf("%s%s", pricePrefix, name)
+	bz := store.Get([]byte(key))
 	var price sdk.Coins
 	k.cdc.MustUnmarshalBinary(bz, &price)
 	return price
@@ -75,6 +85,7 @@ func (k Keeper) GetPrice(ctx sdk.Context, name string) sdk.Coins {
 
 // SetPrice - sets the current price of a name
 func (k Keeper) SetPrice(ctx sdk.Context, name string, price sdk.Coins) {
-	store := ctx.KVStore(k.pricesStoreKey)
-	store.Set([]byte(name), k.cdc.MustMarshalBinary(price))
+	store := ctx.KVStore(k.storeKey)
+	key := fmt.Sprintf("%s%s", pricePrefix, name)
+	store.Set([]byte(key), k.cdc.MustMarshalBinary(price))
 }
